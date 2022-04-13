6a2e1fe5c144ca8205c226612292b354a2689270
testeredekiadam <erkolfirat@gmail.com>
2022-04-13 20:19:46 +0200

    interfaces-shape-color

---
+++ 1_AbstractFactoryMethod/src/main/java/com/firat/Shape.java
@@ -0,0 +1,28 @@
+package com.firat;
+
+
+interface Shape {
+	void draw();
+}
+
+class Rectangle implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+}
+
+class Circle implements Shape {
+	@Override
+	public void draw() {
+	System.out.println("Inside Circle::draw() method.");	
+	}
+}
+
+class Square implements Shape {
+	@Override
+	public void draw() {
+		System.out.println("Inside Square::draw() method.");
+	}
+	
+}
