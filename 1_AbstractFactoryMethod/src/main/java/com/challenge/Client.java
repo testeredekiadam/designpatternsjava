4cec6e83d3bc7dbfb9552fce464b336966a8ff6d
testeredekiadam <erkolfirat@gmail.com>
2022-04-15 18:46:09 +0200

    challenge final

--- 1_AbstractFactoryMethod/src/main/java/com/challenge/Client.java
+++ 1_AbstractFactoryMethod/src/main/java/com/challenge/Client.java
@@ -2,19 +2,30 @@ package com.challenge;
 
 public class Client {
 	public static void main(String[] args) {
-		//get the movie factory
-		AbstractFactory movieFactory = FactoryProducer.getFactory("Hollywood");
+		//get the hollywood movie factory
+		AbstractFactory hMovieFactory = FactoryProducer.getFactory("Hollywood");
 		
 		//get an action movie on hollywood
-		HollywoodMovie hmovie1 = movieFactory.getHollywoodMovie("Action");
+		HollywoodMovie hmovie1 = hMovieFactory.getHollywoodMovie("Action");
 		//call getName() method on hollywood action movie
 		hmovie1.getMovieName();
 		
 		//get a commedy movie on hollywood
-		HollywoodMovie hmovie2 = movieFactory.getHollywoodMovie("Comedy");
+		HollywoodMovie hmovie2 = hMovieFactory.getHollywoodMovie("Comedy");
 		//call getName() method on hollywood comedy
 		hmovie2.getMovieName();
 		
+		//get the bollywood movie factory
+		AbstractFactory bMovieFactory = FactoryProducer.getFactory("Bollywood");
 		
+		//get an action movie on bollywood
+		BollywoodMovie bmovie1 = bMovieFactory.getBollywoodMovie("Action");
+		//call getName() method on bollywood action
+		bmovie1.getMovieName();
+		
+		//get an action movie on bollywood
+		BollywoodMovie bmovie2 = bMovieFactory.getBollywoodMovie("Comedy");
+		//call getName() method on bollywood action
+		bmovie2.getMovieName();
 	}
 }
